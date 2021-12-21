f988655c43ce64e5fd0a8cc48aea8694d440e54e
Janis <janis.zesiger@gmail.com>
2021-12-21 17:01:02 +0100

    Vorlage in Bearbeitung (v2.3)

--- script/script.js
+++ script/script.js
@@ -3,54 +3,52 @@ import * as THREE from './lib/three.module.js'
 import {OrbitControls} from './lib/OrbitControls.js'
 
 
-class BasicWorldDemo {
+class app {
   constructor() {
-    this._Initialize();
+    this.initalize();
   }
 
-  _Initialize() {
-    this._threejs = new THREE.WebGLRenderer({
-      antialias: true,
-    });
-    this._threejs.setPixelRatio(window.devicePixelRatio);
-    this._threejs.setSize(window.innerWidth, window.innerHeight);
+  initalize() {
+    this.renderer = new THREE.WebGLRenderer({antialias: true});
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
 
-    document.body.appendChild(this._threejs.domElement);
+    document.body.appendChild(this.renderer.domElement);
 
     window.addEventListener('resize', () => {
-      this._OnWindowResize();
+      this.resize();
     }, false);
 
-    this._camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
-    this._camera.position.set(75, 20, 0);
+    this.camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
+    this.camera.position.set(75, 20, 0);
 
-    this._scene = new THREE.Scene();
+    this.scene = new THREE.Scene();
 
     const controls = new OrbitControls(
-      this._camera, this._threejs.domElement);
+      this.camera, this.renderer.domElement);
     controls.target.set(0, 20, 0);
     controls.update();
 
-    const geometry = new THREE.PlaneGeometry(100, 100)
-    const material = new THREE.MeshBasicMaterial({color: 0x00ff00})
-    const mesh = new THREE.Mesh(geometry, material)
-    mesh.rotateX(Math.PI + (Math.PI / 2))
+    this.planegeometry = new THREE.PlaneGeometry(100, 100)
+    this.planematerial = new THREE.MeshBasicMaterial({color: 0x00ff00})
+    this.plane = new THREE.Mesh(this.planegeometry, this.planematerial)
+    this.plane.rotateX(Math.PI + (Math.PI / 2))
 
-    this._scene.add(mesh)
+    this.scene.add(this.plane)
 
-    this._RAF();
+    this.raf();
   }
 
-  _OnWindowResize() {
-    this._camera.aspect = window.innerWidth / window.innerHeight;
-    this._camera.updateProjectionMatrix();
-    this._threejs.setSize(window.innerWidth, window.innerHeight);
+  resize() {
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
   }
 
-  _RAF() {
+  raf() {
     requestAnimationFrame(() => {
-      this._threejs.render(this._scene, this._camera);
-      this._RAF();
+      this.renderer.render(this.scene, this.camera);
+      this.raf();
     });
   }
 }
@@ -59,5 +57,7 @@ class BasicWorldDemo {
 let _APP = null;
 
 window.addEventListener('DOMContentLoaded', () => {
-  _APP = new BasicWorldDemo();
+  _APP = new app();
+
 });
+
