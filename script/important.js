8af9b7d39099fffaf17bcd524cc9316efcc593a6
Janis <janis.zesiger@gmail.com>
2022-02-09 21:39:01 +0100

    Coming Soon!

--- script/important.js
+++
@@ -1,7 +0,0 @@
-setTimeout(() => {console.log("Never");}, 2000);
-setTimeout(() => {console.log("gonna");}, 3000);
-setTimeout(() => {console.log("give");}, 3700);
-setTimeout(() => {console.log("you");}, 5400);
-setTimeout(() => {console.log("up!");}, 7000);
-
-
